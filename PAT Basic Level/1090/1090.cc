623cb9fc7d0c2574a6c762794d8895e59b747137
IonizingRadiation <PeterSmith_9@outlook.com>
2019-05-26 20:33:41 +0800

    [1090] partially accepted

---
+++ PAT Basic Level/1090/1090.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <vector>
+
+using SetInt = std::set<int>;
+
+std::map<int, SetInt> Conflict;
+
+
+int main(int argc, char* argv[]) {
+  int N, M;
+  std::cin >> N >> M;
+  
+  for (int i=0; i!=N; ++i) {
+    int _A, _B;
+    std::cin >> _A >> _B;
+    Conflict[_A].insert(_B);
+    Conflict[_B].insert(_A);
+  }
+
+  for (int i=0; i!=M; ++i) {
+    SetInt goodlist;
+    int length;
+    std::cin >> length;
+    for (int j=0; j!=length; ++j) {
+      int good;
+      std::cin >> good;
+      goodlist.insert(good);
+    }
+
+    bool isFound = false;
+    for (const int e : goodlist) {
+      /*
+       * if (!isFound and goodlist.end() != goodlist.find(Conflict[e])) {
+       *   isFound = true;
+       *   std::cout << "Yes" << std::endl;
+       * }
+       */
+      std::vector<int> v_intersection;
+      std::set_intersection(goodlist.begin(), goodlist.end(),
+                            Conflict[e].begin(), Conflict[e].end(),
+                            std::back_inserter(v_intersection));      
+      if (!isFound and 0 != v_intersection.size()) {
+        isFound = true;
+        std::cout << "No" << std::endl;
+      }
+    }
+    if (!isFound) {
+      std::cout << "Yes" << std::endl;
+    }
+  }
+  
+  return 0;
+}
